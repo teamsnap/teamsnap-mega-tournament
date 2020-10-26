154e93b09e4522ce57749b4f48c83fb31fa6222c
Jacob Kastensmidt <jksmidt@gmail.com>
2020-10-26 17:32:21 -0400

    Removing unnecessary calls and correcting team display

--- frontend/src/pages/Teams.tsx
+++ frontend/src/pages/Teams.tsx
@@ -2,20 +2,14 @@ import React from 'react';
 import TeamCard from '../components/TeamCard';
 
 
-const Teams: React.FC<{teams: any[]} & {getTeams: any}> = ({teams, getTeams}) => {
+const Teams: React.FC<{ teams: any[] }> = ({ teams }) => {
   const [cards, setCards] = React.useState<any>([]);
-  React.useEffect(() => {
-    setCards([]);
-  }, [teams]);
+
 
   React.useEffect(() => {
-    getTeams();
-  }, []);
-
-  React.useEffect(() => {
-    teams?.forEach((team) => {
-      setCards([...cards, <TeamCard teamName={team.teamName} />]);
-    });
+    setCards(teams?.map((team) => {
+      return <TeamCard teamName={team.teamName} captain={team.captain} key={team.teamName} />
+    }));
   }, [teams]);
 
   return (
