3bcbe6fd9550b95ee785ea2f7359b6e3510f6c8b
Jacob Kastensmidt <jksmidt@gmail.com>
2020-10-26 17:30:18 -0400

    Adding fields for captain first and last name

--- frontend/src/components/NewTeamForm.tsx
+++ frontend/src/components/NewTeamForm.tsx
@@ -1,23 +1,37 @@
 import React from 'react';
+import '../App.css';
 
 interface Props {
   submitTeam: any,
   baseurl: string,
 }
 
-const NewTeamForm: React.FC<Props> = ({submitTeam, baseurl}) => {
-  const [name, setName] = React.useState("");
+const NewTeamForm: React.FC<Props> = ({ submitTeam, baseurl }) => {
+  const [teamName, setTeamName] = React.useState("");
+  const [firstName, setFirstName] = React.useState("");
+  const [lastName, setLastName] = React.useState("");
 
   return (
     <div className="card">
       <h4>New Team</h4>
       <label>
         Name:
-        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        <input type="text" value={teamName} onChange={(e) => setTeamName(e.target.value)} />
       </label>
+      <h5>Team Captain</h5>
+      <div className="u-flexColumn">
+        <label className="u-marginBottom-small">
+          First Name:
+          <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+        </label>
+        <label className="u-marginBottom-small">
+          Last Name:
+          <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+        </label>
+      </div>
       <input type="submit" onClick={(e) => {
         e.preventDefault();
-        submitTeam({url: baseurl + "/new-team", name: name});
+        submitTeam({ url: baseurl + "/teams", team: teamName, firstName: firstName, lastName: lastName });
       }} />
     </div>
   );
