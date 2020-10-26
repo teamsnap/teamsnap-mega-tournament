98ef8f33ea35c8381e7a12a4af1b53949c25c905
Jacob Kastensmidt <jksmidt@gmail.com>
2020-10-26 17:28:55 -0400

    Removing reducers and actions that will no longer be used

--- frontend/src/reducers.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function rootReducer(state: any = {}, action: any) {
-  switch (action.type) {
-    case "UPDATE":
-      return Object.assign({}, state, {[action.key]: action.value});
-    default:
-      return state
-  }
-}
