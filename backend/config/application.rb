fd395fa61d1280b56467eae6dfb1603b40127bbc
Steven Aldinger <stevenaldinger@gmail.com>
2021-02-23 17:05:53 -0500

    halp

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -2,6 +2,13 @@ require_relative 'boot'
 
 require 'rails/all'
 
+# require 'action_controller/railtie'
+# require 'action_mailer/railtie'
+# require 'active_model/railtie'
+# require 'sprockets/railtie'
+# require 'rails/test_unit/railtie'
+# require "active_storage/engine"
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
