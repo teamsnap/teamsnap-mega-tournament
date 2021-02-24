41dae5138d819c78f5db59d20174834abb307332
Ryan Williams <ryan.williams@teamsnap.com>
2021-02-23 16:03:59 -0800

    Restore routes/controller

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'greetings/hello'
+  get '/teams', to: 'teams#index'
+  post '/teams', to: 'teams#create'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
